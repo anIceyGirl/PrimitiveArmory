4f63531755979b9db2136918b4a8377148bb4b10
Icey <Penguixia@gmail.com>
2020-12-04 06:38:20 -0500

    New sounds  (not implemented yet but i'm working on it)

---
+++ enum/EnumExt_Sounds.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PrimitiveArmory
+{
+    public static class EnumExt_Sounds
+    {
+        public static SoundID Slugcat_Bow_Pickup;
+
+        public static SoundID Weapon_Bow_Draw;
+        public static SoundID Weapon_Bow_Fire;
+        public static SoundID Weapon_Bow_FireDry;
+
+        public static SoundID Bottle_Slosh;
+        public static SoundID Bottle_Smash;
+        
+    }
+}
